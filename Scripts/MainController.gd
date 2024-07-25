b88164585db5cced066ec5fe04d6010eda2c0635
Thomas Snyder <tomsamsny@gmail.com>
2024-07-25 18:37:42 -0400

    Added basic camera controls

--- Scripts/MainController.gd
+++ Scripts/MainController.gd
@@ -5,3 +5,9 @@ extends Node2D
 func _ready():
 	var level = SceneDefs.level01.instantiate()
 	add_child(level)
+	
+	# Turn off the mouse cursor when inside the window
+	# Eventually this will need to be turned back on when hovering over
+	# UI panels/elements, but this is better for now
+	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_HIDDEN)
+ 
