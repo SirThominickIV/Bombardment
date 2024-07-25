503fe8d568cba7c731239c5f80e530a243137d70
Thomas Snyder <52889852+SirThominickIV@users.noreply.github.com>
2024-07-25 18:38:43 -0400

    Feature 3 add camera controls (#14)
    * Added main scene
    
    * Added basic camera controls
    
    * Mouse cursor is now hidden (Should later be toggled when UI elements become a thing)

--- Scripts/Definitions/SceneDefs.gd
+++ Scripts/Definitions/SceneDefs.gd
@@ -1,6 +1,19 @@
 extends Node
 
+
+
 const StandardArtillery = preload("res://Scenes/standardArtillery.tscn")
 const Nuke = preload("res://Scenes/nuke.tscn")
 const Explosion = preload("res://Scenes/explosion.tscn")
 const NuclearExplosion = preload("res://Scenes/nuclearExplosion.tscn")
+
+
+# Preloading requires a path to be a constant string, so each
+# level needs to be it's own const. This would have been the prefered
+# solution:
+#func GetLevel(level):
+	#var format_string = "res://Scenes/Levels/level%s.tscn"
+	#var path = format_string % level.to_string()
+	#return preload(path)
+	
+const level01 = preload("res://Scenes/Levels/level01.tscn")
