b468d8e684292f47edb120659b0514fd02392797
Thomas Snyder <tomsamsny@gmail.com>
2025-06-08 13:45:12 -0400

    Node cull renames

--- Scripts/NodeCull.gd
+++ Scripts/NodeCull.gd
@@ -1,9 +1,9 @@
 extends Node
 class_name NodeCull
 
-@export var TicksUntilCull = 200.0
+@export var ticks_until_cull = 200.0
 
 func _physics_process(_delta):
-	TicksUntilCull -= 1
-	if(TicksUntilCull < 0):
+	ticks_until_cull -= 1
+	if(ticks_until_cull < 0):
 		queue_free()
